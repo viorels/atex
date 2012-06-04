15efd6f74e9073b4d85a71142a0e376f8374df73
Viorel Stirbu <viorels@gmail.com>
2012-06-04 20:11:49 +0300

    also add static resources

--- atexpc/atex_frontend/urls.py
+++ atexpc/atex_frontend/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^products/', views.products, name='products'),
+    url(r'^$', views.home, name='home'),
+    url(r'^produse/', views.products, name='products'),
 )
--- atexpc/atex_frontend/views.py
+++ atexpc/atex_frontend/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
+
+def home(request):
+    context = {}
+    return render(request, "home.html", context)
 
 def products(request):
     products = [{'id': 1, 'name': 'Procesoare'},
                 {'id': 2, 'name': 'Placi de baza'}]
-    context = {'products': products}
+    context = {'categories': products}
     return render(request, "products.html", context)
