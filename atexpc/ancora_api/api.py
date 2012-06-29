cfe6dbfdcd743554f6eb82581b28d5c594cd8b53
Viorel Stirbu <viorels@gmail.com>
2012-06-29 08:41:51 +0300

    replace timeless memoize with django 5 min expiry django cache backend

--- atexpc/ancora_api/api.py
+++ atexpc/ancora_api/api.py
@@ -5,7 +5,7 @@ import json
 from urlparse import urlparse, urlunparse, parse_qsl
 from urllib import urlencode
 from urllib2 import urlopen # TODO: try urllib3 with connection pooling
-from cPickle import dumps, PicklingError # for memoize
+from django.core.cache import cache
 
 import logging
 logger = logging.getLogger(__name__)
@@ -13,45 +13,12 @@ logger = logging.getLogger(__name__)
 # TODO: this doesn't work if the lib is packed in an egg
 MOCK_DATA_PATH = os.path.join(os.path.split(__file__)[0], 'mock_data')
 
-class memoize(object):
-    """Decorator that caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned, and
-    not re-evaluated. Slow for mutable types."""
-    # Ideas from MemoizeMutable class of Recipe 52201 by Paul Moore and
-    # from memoized decorator of http://wiki.python.org/moin/PythonDecoratorLibrary
-    # For a version with timeout see Recipe 325905
-    # For a self cleaning version see Recipe 440678
-    # Weak references (a dict with weak values) can be used, like this:
-    #   self._cache = weakref.WeakValueDictionary()
-    #   but the keys of such dict can't be int
-    def __init__(self, func):
-        self.func = func
-        self._cache = {}
-    def __call__(self, *args, **kwds):
-        key = args
-        if kwds:
-            items = kwds.items()
-            items.sort()
-            key = key + tuple(items)
-        try:
-            if key in self._cache:
-                return self._cache[key]
-            self._cache[key] = result = self.func(*args, **kwds)
-            return result
-        except TypeError:
-            try:
-                dump = dumps(key)
-            except PicklingError:
-                return self.func(*args, **kwds)
-            else:
-                if dump in self._cache:
-                    return self._cache[dump]
-                self._cache[dump] = result = self.func(*args, **kwds)
-                return result
-
-@memoize
 def _read_uri(uri):
-    return urlopen(uri).read()
+    response = cache.get(uri)
+    if response is None:
+        response = urlopen(uri).read()
+        cache.set(uri, response)
+    return response
 
 class BaseAdapter(object):
     def __init__(self, base_uri=None):
