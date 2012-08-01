9f570d667d382cab28ed4e051ce0e2e7c74d99be
Viorel Stirbu <viorels@gmail.com>
2012-08-02 00:11:32 +0300

    remove sqlite3 from runtime config as it fails on heroku

--- atexpc/atexpc/settings.py
+++ atexpc/atexpc/settings.py
@@ -1,3 +1,4 @@
+import sys
 import os
 
 # Django settings for atexpc project.
@@ -13,7 +14,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -21,6 +22,9 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CACHES = {
     'default': {
