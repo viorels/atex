1353f3a620c928e0b0a5d247742768f761320ed8
Viorel Stirbu <viorels@gmail.com>
2012-08-02 23:50:50 +0300

    use gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python atexpc/manage.py runserver 0:$PORT
+web: python atexpc/manage.py run_gunicorn -b 0.0.0.0:$PORT -w 3
--- atexpc/atexpc/settings.py
+++ atexpc/atexpc/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'atex_frontend',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
