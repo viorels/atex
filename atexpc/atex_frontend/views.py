12cd59eff7cb774b55a8ca701088e8f4e1af1c19
Viorel Stirbu <viorels@gmail.com>
2012-08-05 02:39:00 +0300

    only show selectors with count greater then 0

--- atexpc/atex_frontend/views.py
+++ atexpc/atex_frontend/views.py
@@ -193,7 +193,14 @@ def _get_menu():
 def _get_selectors(category_id, selectors_active):
     selector_groups = ancora.get_selectors(category_id=category_id,
                                            selectors_active=selectors_active)
-    return selector_groups
+    useful_selector_groups = []
+    for selector_group in selector_groups:
+        useful_selectors = [selector for selector in selector_group['selectors']
+                            if int(selector['count']) > 0]
+        if len(useful_selectors) > 0:
+            useful_selector_groups.append({'name': selector_group['name'],
+                                           'selectors': useful_selectors})
+    return useful_selector_groups
 
 def _get_footer():
     return [{'name': category['name'],
