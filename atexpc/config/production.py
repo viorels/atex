cb4dda42d317510ef863bbd4597138b859d800c1
Atex <atex@ziuadecjopenx.efect.local>
2012-09-19 05:00:39 -0400

    production configuration

---
+++ atexpc/config/production.py
@@ -0,0 +1,26 @@
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'atexpc.atex_web',
+    'gunicorn',
+    'south',
+    'storages',
+    'sorl.thumbnail',
+)
+
+STATIC_URL = "/static/"
+MEDIA_URL = STATIC_URL
+
