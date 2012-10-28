c3e158e95f410869c8876d4c3404e3d08d8424d9
Viorel Stirbu <viorels@gmail.com>
2012-10-28 20:15:50 +0200

    add beta notification to "adauga in cos" button on product page

--- atexpc/atex_web/static/script/all.js
+++ atexpc/atex_web/static/script/all.js
@@ -176,7 +176,7 @@ $(document).ready(function()
 
 function init_beta() {
     $(".login_holder li, .info_cos li, .search_bar .configurator, " +
-      ".product_holder .add_cos_btn, .email-wrapper .email_btn").click(work_in_progress);
+      ".add_cos_btn, .email-wrapper .email_btn").click(work_in_progress);
 }
 
 function work_in_progress() {
