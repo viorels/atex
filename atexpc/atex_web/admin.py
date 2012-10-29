d5d30d725f83cd4396980b5ddd5501c8b80f99fc
Viorel Stirbu <viorels@gmail.com>
2012-10-29 03:40:09 +0200

    only create dropbox folders for products without images

--- atexpc/atex_web/admin.py
+++ atexpc/atex_web/admin.py
@@ -33,7 +33,8 @@ class ProductAdmin(admin.ModelAdmin):
     def create_dropbox_folder(self, request, queryset):
         dropbox = DropboxMedia()
         for product in queryset:
-            dropbox.create_product_folder(product.folder_name())
+            if len(product.image_files()) == 0:
+                dropbox.create_product_folder(product.folder_name())
         self.message_user(request, "Created %d product folders on Dropbox" % len(queryset))
     create_dropbox_folder.short_description = "Create Dropbox folders for selected products"
 
