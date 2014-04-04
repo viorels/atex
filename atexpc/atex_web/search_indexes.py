b870eff96ae48094468b3253216c69a1e6c9c295
Viorel Stirbu <viorels@gmail.com>
2014-04-04 16:23:08 +0300

    add stock to search index ?

--- atexpc/atex_web/search_indexes.py
+++ atexpc/atex_web/search_indexes.py
@@ -7,6 +7,7 @@ class ProductIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     category = indexes.CharField(model_attr='category')
     updated = indexes.DateTimeField(model_attr='updated')
+    # stock = indexes.BooleanField(model_attr='stock')
 
     def get_model(self):
         return Product
